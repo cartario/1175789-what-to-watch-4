9cbdf98eabf2899b635760d89265c1f2d09bf637
Vasiliy <cartario@yandex.ru>
2020-07-09 17:01:22 +0300

    правки хока, подправил player

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,14 +1,54 @@
-import React, {Fragment} from 'react';
+import React, {PureComponent, createRef} from 'react';
 import PropTypes from "prop-types";
 import withVideo from "../../hocs/with-video/with-video.js";
 
-const Player = (props) => {
-  return (
-    <Fragment>
-      {props.children}
-    </Fragment>
-  );
-};
+
+class Player extends PureComponent {
+  constructor() {
+    super();
+    this._videoRef = createRef();
+    this._timeout = null;
+  }
+
+  componentDidMount() {
+    const {film, isMuted} = this.props;
+    const video = this._videoRef.current;
+    video.src = film.preview;
+    video.poster = film.src;
+    video.width = `280`;
+    video.height = `175`;
+    video.muted = isMuted;
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.props.isPlaying) {
+      this._timeout = setTimeout(() => {
+        video.play();
+      }, 1000);
+
+    } else {
+      clearTimeout(this._timeout);
+      video.load();
+    }
+  }
+
+  componentWillUnmount() {
+    const video = this._videoRef.current;
+    video.src = null;
+    video.poster = null;
+    video.width = null;
+    video.height = null;
+    video.muted = null;
+  }
+
+  render() {
+    return (
+      <video ref = {this._videoRef}/>
+    );
+  }
+}
 
 Player.propTypes = {
   film: PropTypes.shape({
@@ -17,10 +57,7 @@ Player.propTypes = {
     preview: PropTypes.string.isRequired,
   }),
   isPlaying: PropTypes.bool.isRequired,
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
+  isMuted: PropTypes.bool.isRequired,
 };
 
 export default withVideo(Player);
--- src/hocs/with-video/with-video.js
+++ src/hocs/with-video/with-video.js
@@ -1,59 +1,23 @@
-import React, {PureComponent, createRef} from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from "prop-types";
 
 const withVideo = (Component) => {
   class WithVideo extends PureComponent {
     constructor(props) {
       super(props);
-      this._videoRef = createRef();
+
       this.state = {
-        isPause: !this.props.isPlaying,
+        isPause: true,
         isMuted: true,
       };
-      this._timeout = null;
-    }
-
-    componentDidUpdate() {
-      const video = this._videoRef.current;
-
-      if (this.props.isPlaying) {
-        this._timeout = setTimeout(() => {
-          video.play();
-
-          this.setState({
-            isPause: false,
-          });
-
-        }, 1000);
-
-
-      } else {
-        clearTimeout(this._timeout);
-        video.load();
-
-        this.setState({
-          isPause: true,
-        });
-      }
     }
 
     render() {
-
-      const {film} = this.props;
       const {isPause, isMuted} = this.state;
-
       return (
-        <Component {...this.props } isPause={isPause} isMuted={isMuted}>
-
-          <video
-            src={film.preview}
-            poster={film.src}
-            muted = {this.state.isMuted}
-            ref = {this._videoRef}
-            width="280"
-            height="175">
-          </video>
-        </Component>
+        <Component {...this.props }
+          isPause={isPause} isMuted={isMuted}
+        />
       );
     }
   }
