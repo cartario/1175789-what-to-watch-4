183e029f843239ac99fe985589149373ab7940fa
Vasiliy <cartario@yandex.ru>
2020-08-02 18:47:47 +0300

    тест-Overview

--- src/components/full-player/full-player.test.js
+++ src/components/full-player/full-player.test.js
@@ -1,43 +1,46 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import {Provider} from 'react-redux';
-// import configureStore from 'redux-mock-store';
-// import {Router} from 'react-router-dom';
-// import {history} from "../../history.js";
-// import FullPlayer from "./full-player.jsx";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import {Router} from 'react-router-dom';
+import {history} from "../../history.js";
+import FullPlayer from "./full-player.jsx";
 
-// const mockStore = configureStore([]);
+const mockStore = configureStore([]);
 
-// describe(`FullVideoPlayer`, () => {
-//   const store = mockStore({
-//     FILMS: {
-//       filmsByGenre: [],
-//       id: `1`,
-//       posterImage: ``,
-//     }
+describe(`FullVideoPlayer`, () => {
+  const store = mockStore({
+    FILMS: {
+      films: [
+        {
+          filmsByGenre: [],
+          id: `1`,
+          posterImage: ``,
+        }
+      ],
+    },
 
-//   });
+  });
 
-//   const match = {params:
-//     {
-//       id: `1`,
-//     }};
 
-//   it(`should render FullPlayer`, () => {
-//     const tree = renderer.create(
-//         <Router history = {history}>
-//           <Provider store = {store}>
-//             <FullPlayer
-//               match = {match}
-//             />
-//           </Provider>
-//         </Router>, {
-//           createNodeMock: () => {
-//             return {};
-//           }
-//         }
-//     ).toJSON();
+  it(`should render FullPlayer`, () => {
+    const tree = renderer.create(
+        <Router history = {history}>
+          <Provider store = {store}>
+            <FullPlayer
+              match = {{params:
+                {
+                  id: `1`,
+                }}}
+            />
+          </Provider>
+        </Router>, {
+          createNodeMock: () => {
+            return {};
+          }
+        }
+    ).toJSON();
 
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
+    expect(tree).toMatchSnapshot();
+  });
+});
