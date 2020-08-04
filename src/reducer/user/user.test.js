3b4430b9a0d4ae69d593935e2a71d37344135e08
cartario <cartario@yandex.ru>
2020-08-04 10:58:51 +0300

    test: reducer User

---
+++ src/reducer/user/user.test.js
@@ -0,0 +1,9 @@
+import {reducer} from "./user.js";
+
+describe(`test-reducer-user`, () => {
+  it(`should return initial state`, () => {
+    expect(reducer(void 0, {})).toEqual({
+      authorizationStatus: `NO_AUTH`,
+    });
+  });
+});
